b8ab306d18f7bde4c3e12322d9bbb5853b1f7ae6
Luka M <164436043+ghluka@users.noreply.github.com>
2026-04-19 22:03:05 -0400

    docs

---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Grizzly Docs',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://ghluka.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/MedvedClient/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ghluka', // Usually your GitHub org/user name.
+  projectName: 'MedvedClient', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: '../docs',
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/social-card.jpg',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false
+      },
+      navbar: {
+        //title: 'My Site',
+        logo: {
+          alt: 'Grizzly Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'search',
+            position: 'right',
+          },
+          //{
+          //  type: 'docSidebar',
+          //  sidebarId: 'tutorialSidebar',
+          //  position: 'left',
+          //  label: 'Tutorial',
+          //},
+          //{to: '/blog', label: 'Blog', position: 'left'},
+          //{
+          //  href: 'https://github.com/facebook/docusaurus',
+          //  label: 'GitHub',
+          //  position: 'right',
+          //},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Grizzly',
+                to: '/',
+              },
+            ],
+          },
+          //{
+          //  title: 'Community',
+          //  items: [
+          //    {
+          //      label: 'Stack Overflow',
+          //      href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //    },
+          //    {
+          //      label: 'Discord',
+          //      href: 'https://discordapp.com/invite/docusaurus',
+          //    },
+          //    {
+          //      label: 'X',
+          //      href: 'https://x.com/docusaurus',
+          //    },
+          //  ],
+          //},
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Homepage',
+                to: 'https://ghluka.github.io/medved',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ghluka/MedvedClient',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ghluka. Built with Docusaurus.`,
+      },
+      prism: {
+        //theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        language: ['en'],
+      },
+    ],
+  ],
+};
+
+export default config;
